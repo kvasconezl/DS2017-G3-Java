ab0f49c40c2a9c2308fd0674bb2a9246cb51f90b
Kevin Vásconez <kvasconezl@gmail.com>
2017-08-22 06:08:02 -0500

    AdminWindow - Complete

--- CatalogoDeDelicias/src/main/FXMLAdminMenuController.java
+++ CatalogoDeDelicias/src/main/FXMLAdminMenuController.java
@@ -5,11 +5,19 @@
  */
 package main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.apache.commons.lang3.text.WordUtils;
 
 /**
@@ -25,7 +33,16 @@ public class FXMLAdminMenuController implements Initializable {
     private Label restaurante;
     @FXML
     private Label privilegios;
-    private String fxml;
+    @FXML
+    private Button salir;
+    @FXML
+    private Button addRest;
+    @FXML
+    private Button listRest;
+    @FXML
+    private Button addStaff;
+    @FXML
+    private Button addUser;
 
     public void mostrarUsuario() {
         String name = WordUtils.capitalizeFully(Main.getACTUAL_USER().getNombre());
@@ -33,6 +50,29 @@ public class FXMLAdminMenuController implements Initializable {
         nombre.setText(name);
         restaurante.setText(rest);
         privilegios.setText(Main.getACTUAL_USER().getTipo());
+    }
+
+    public void changeWindow(ActionEvent event) throws IOException {
+        Button b = ((Button) event.getSource());
+        String fxml;
+        if (b.equals(salir)) {
+            fxml = "FXMLLogin.fxml";
+        } else if (b.equals(addRest)) {
+            fxml = "FXMLAddRestaurant.fxml";
+        } else if (b.equals(addUser)) {
+            fxml = "FXMLAddUser.fxml";
+        } else if (b.equals(addStaff)) {
+            fxml = "FXMLAddStaff.fxml";
+        } else {
+            fxml = "FXMLListRestaurant.fxml";
+        }
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Catálogo de Delicias");
+        stage.show();
+        ((Node) (nombre)).getScene().getWindow().hide();
     }
 
     /**
